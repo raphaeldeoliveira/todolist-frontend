df81ab02415827454a776d1ed6f92f33598fc9ba
raphaeldeoliveira <rafaolive3003@gmail.com>
2024-01-21 22:52:34 -0300

    inicio da implementação da pagina de login

---
+++ src/App.js
@@ -0,0 +1,13 @@
+import Header from "./components/Header";
+import Login from "./pages/Login";
+import "./main.scss"
+
+function App() {
+  return (
+    <div className="App">
+      <Login />
+    </div>
+  );
+}
+
+export default App;
---
+++ src/pages/Login.jsx
@@ -0,0 +1,19 @@
+import React from "react";
+
+export default function Login() {
+
+
+    return (
+        <div className="login">
+            <div className="left-page">
+a
+            </div>
+            <form className="right-page">
+                <label>user: </label>
+                <input type="text" placeholder="username"/>
+                <label>password: </label>
+                <input type="text" placeholder="password"/>
+            </form>
+        </div>
+    )
+}
